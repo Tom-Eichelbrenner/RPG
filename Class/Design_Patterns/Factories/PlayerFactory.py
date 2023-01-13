a98a936fc53c54f8fa0d640f88cf135d64802ec6
FanZy0127 <fanzy28@hotmail.fr>
2023-01-13 12:09:43 +0100

    Changement de la PlayerFactory.

--- Class/Design_Patterns/Factories/PlayerFactory.py
+++ Class/Design_Patterns/Factories/PlayerFactory.py
@@ -4,10 +4,9 @@ from RPG.Class.Entities.Player import Player
 
 class PlayerFactory(EntityFactory):
     @staticmethod
-    def create(name, level, hp):
-        if not name or not level or not hp:
-            raise ValueError("All arguments are required.")
-        if not isinstance(level, int) or not isinstance(hp, int):
-            raise ValueError("Level and hp must be integers.")
+    def create(name, level=1, hp=100):
+        if not name:
+            raise ValueError("A name is required.")
+
         return Player(name, level, hp)
 
